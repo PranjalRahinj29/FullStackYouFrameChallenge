374fcc05b3f39d6bb98a28decade601458523a68
Pranjal Rahinj <pranjalbhand2020@gmail.com>
2020-10-11 14:59:10 +0530

    delete test component added for testing setup and only keep that is require

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,40 +5,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HelloService } from './hello/hello.service';
 import { RouterModule, Route } from '@angular/router';
 import { UploadComponent } from './upload/upload.component';
 import { NgImageSliderModule } from 'ng-image-slider';
-import { DummyComponent } from './dummy/dummy.component';
-import { SliderComponent } from './slider/slider.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 const routes: Route[] = [
- {path:'app-hello', component:HelloComponent},
- {path:'app-upload', component:UploadComponent},
- {path:'app-slider', component: SliderComponent}
+
+  { path: 'app-upload', component: UploadComponent }
+
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    UploadComponent,
-    DummyComponent,
-    SliderComponent
+    UploadComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgImageSliderModule,
     BrowserAnimationsModule,
-   RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes)
   ],
-  providers: [HelloService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,46 +1,104 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgImageSliderModule } from 'ng-image-slider';
+import { Component, OnInit, SecurityContext, OnChanges, SimpleChanges } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+  keyframes
+} from '@angular/animations';
 
 @Component({
-    selector: 'app-upload',
-    templateUrl: 'upload.component.html',
-    styleUrls: ['/upload.component.css']
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.css'],
+  animations: [
+    trigger('move', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => left', [
+        style({ transform: 'translateX(100%)' }),
+        animate(200)
+      ]),
+      transition('left => void', [
+        animate(200, style({ transform: 'translateX(0)' }))
+      ]),
+      transition('void => right', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(200)
+      ]),
+      transition('right => void', [
+        animate(200, style({ transform: 'translateX(0)' }))
+      ])
+    ])
+  ]
 })
-
-
 export class UploadComponent implements OnInit {
-    ImagePath: string;
 
-    imageObject: Array<object> = [{
-        image: '/assets/abc.jpg',
-        thumbImage: '/assets/abc.jpg'
-        //,alt: '1', // optional
-        //title: '1' //optional
-    }, {
-        image: '/assets/apple_laptop.jpeg',
-        thumbImage: '/assets/apple_laptop.jpeg'
-    }, {
-        image: '/assets/apple_mobile.jpeg',
-        thumbImage: '/assets/apple_mobile.jpeg'
-    }, {
-        image: '/assets/bb.jpg',
-        thumbImage: '/assets/bb.jpg'
-    }, {
-        image: '/assets/PranjalPhoto_1.jpg',
-        thumbImage: '/assets/PranjalPhoto_1.jpg'
-    }, {
-        image: '/assets/SemiformalResume_pic.jpg',
-        thumbImage: '/assets/SemiformalResume_pic.jpg'
-    }
+  images = [];
+
+  public safeUrls = [];
+  public imageUrls: any;
+  public state = 'void';
+  public disableSliderButtons = false;
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+
+    this.images = [
+      '/assets/images/apple_mobile.jpeg', 
+      '/assets/images/bb.jpg', 
+      '/assets/images/electronic1.jpg',
+      '/assets/images/electronic2.jpg',
+      '/assets/images/apple_laptop.jpeg'
     ];
 
-    imageSize: Object = { width: 500, height: 400, space: 10 };
+    this.images.forEach(element => {
+      const safeUrl = this.sanitizer.sanitize(SecurityContext.URL, element);
+      this.safeUrls.push(safeUrl);
+    });
 
-    constructor() {
-        this.ImagePath = '/assets/apple_laptop.jpeg'
+    this.imageUrls = this.safeUrls;
+  }
+
+  onUpload(event) {
+    this.imageUrls.unshift('/assets/images/abc.jpg');
+    
+  }
+
+  imageRotate(arr, reverse) {
+    if (reverse) {
+      arr.unshift(arr.pop());
+    } else {
+      arr.push(arr.shift());
     }
+    return arr;
+  }
 
-    ngOnInit() { }
+  moveLeft() {
+    if (this.disableSliderButtons) {
+      return;
+    }
+    this.state = 'right';
+    this.imageRotate(this.imageUrls, true);
+  }
+
+  moveRight() {
+    if (this.disableSliderButtons) {
+      return;
+    }
+    this.state = 'left';
+    this.imageRotate(this.imageUrls, false);
+  }
+
+  onFinish($event) {
+    this.state = 'void';
+    this.disableSliderButtons = false;
+  }
+
+  onStart($event) {
+    this.disableSliderButtons = true;
+  }
 }
-
